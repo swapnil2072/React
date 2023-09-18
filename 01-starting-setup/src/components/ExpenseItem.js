f02fd808132cff4750c635f6478c34040a704b13
swapnil2072 <swapnilshinde909693@gmail.com>
2023-09-18 15:15:00 +0530

    adding css styles

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
-      <div>March 28th 2023</div>
-      <div>
+    <div className="expense-item">
+      <div >March 28th 2023</div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>20000</div>
+        <div className="expense-item__price">20000</div>
       </div>
     </div>
   );
