137ab68fbd0120db1c167a297e2e650d968fc87a
swapnil2072 <swapnilshinde909693@gmail.com>
2023-09-18 15:27:29 +0530

    making data dynamic

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2023,9,15);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 20000;
   return (
     <div className="expense-item">
-      <div >March 28th 2023</div>
+      <div >{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">20000</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">Rs{expenseAmount}</div>
       </div>
     </div>
   );
