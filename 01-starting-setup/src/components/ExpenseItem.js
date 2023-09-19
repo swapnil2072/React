c519c69a0d6da5e81e460621386690ac8bc4e523
swapnil2072 <swapnilshinde909693@gmail.com>
2023-09-19 12:16:41 +0530

    spliting expenseitem component into expenseDate component

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,9 @@
 import "./ExpenseItem.css";
-
+import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
-  const day = props.date.toLocaleString('en-US',{day:'2-digit'});
-  const month = props.date.toLocaleString('en-US',{month:'long'});
-  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">Rs{props.amount}</div>
