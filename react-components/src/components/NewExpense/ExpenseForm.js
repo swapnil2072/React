eccf7a322bc058d967b91187adc4916977955f72
swapnil2072 <swapnilshinde909693@gmail.com>
2023-09-27 21:16:38 +0530

    using mutliple states

--- react-components/src/components/NewExpense/ExpenseForm.js
+++ react-components/src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <title>Title</title>
-          <input type="text"></input>
+          <input type="text" onChange={titleChangeHandler}></input>
         </div>
         <div className="new-expense__control">
           <title>Amount</title>
-          <input type="number" min="0.1" step="0.1"></input>
+          <input
+            type="number"
+            min="0.1"
+            step="0.1"
+            onChange={amountChangeHandler}
+          ></input>
         </div>
         <div className="new-expense__control">
           <title>Date</title>
-          <input type="date" min="2019-01-01" max="2023-12-31"></input>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-12-31"
+            onChange={dateChangeHandler}
+          ></input>
         </div>
       </div>
       <div className="new-expense__actions">
