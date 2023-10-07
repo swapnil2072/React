873e9ae0f90ec112189e5a630c7264c4cd110bb9
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-07 19:13:51 +0530

    adding chart

--- react-components/src/components/Chart/Chart.js
+++ react-components/src/components/Chart/Chart.js
@@ -3,7 +3,18 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
-    return 
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        ></ChartBar>
+      ))}
+    </div>
+  );
 };
 
 export default Chart;
