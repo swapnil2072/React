b7c6c7041f56595b1d6598f6234c3306abd50549
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-15 16:38:20 +0530

    handling events

--- practice-projects/src/components/UserInput/UserInput.js
+++ practice-projects/src/components/UserInput/UserInput.js
@@ -1,40 +1,74 @@
-import React from 'react'
+import React from "react";
+
+const submitHandler = (event) => {
+  event.preventDefault();
+};
+
+const resetHandler = (event) => {};
+
+const inputChangeHandler = (input, value) => {};
 
 const UserInput = () => {
   return (
-    <div><form className="form">
-    <div className="input-group">
-      <p>
-        <label htmlFor="current-savings">Current Savings ($)</label>
-        <input type="number" id="current-savings" />
-      </p>
-      <p>
-        <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-        <input type="number" id="yearly-contribution" />
-      </p>
+    <div>
+      <form onSubmit={submitHandler} className="form">
+        <div className="input-group">
+          <p>
+            <label htmlFor="current-savings">Current Savings ($)</label>
+            <input
+              onChange={(event) =>
+                inputChangeHandler("current-savings", event.target.value)
+              }
+              type="number"
+              id="current-savings"
+            />
+          </p>
+          <p>
+            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+            <input
+              onChange={(event) =>
+                inputChangeHandler("yearly-contribution", event.target.value)
+              }
+              type="number"
+              id="yearly-contribution"
+            />
+          </p>
+        </div>
+        <div className="input-group">
+          <p>
+            <label htmlFor="expected-return">
+              Expected Interest (%, per year)
+            </label>
+            <input
+              onChange={(event) =>
+                inputChangeHandler("expected-return", event.target.value)
+              }
+              type="number"
+              id="expected-return"
+            />
+          </p>
+          <p>
+            <label htmlFor="duration">Investment Duration (years)</label>
+            <input
+              onChange={(event) =>
+                inputChangeHandler("duration", event.target.value)
+              }
+              type="number"
+              id="duration"
+            />
+          </p>
+        </div>
+        <p className="actions">
+          <button onClick={resetHandler} type="reset" className="buttonAlt">
+            Reset
+          </button>
+          <button type="submit" className="button">
+            Calculate
+          </button>
+        </p>
+      </form>
     </div>
-    <div className="input-group">
-      <p>
-        <label htmlFor="expected-return">
-          Expected Interest (%, per year)
-        </label>
-        <input type="number" id="expected-return" />
-      </p>
-      <p>
-        <label htmlFor="duration">Investment Duration (years)</label>
-        <input type="number" id="duration" />
-      </p>
-    </div>
-    <p className="actions">
-      <button type="reset" className="buttonAlt">
-        Reset
-      </button>
-      <button type="submit" className="button">
-        Calculate
-      </button>
-    </p>
-  </form></div>
-  )
-}
+  );
+};
 
-export default UserInput
+export default UserInput;
