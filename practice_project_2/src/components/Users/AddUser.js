409facf55b24d682edf99bb33dd8ea44b571d002
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-21 13:18:05 +0530

    managing error state

--- practice_project_2/src/components/Users/AddUser.js
+++ practice_project_2/src/components/Users/AddUser.js
@@ -7,6 +7,7 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState("");
   const [enterUserAge, setEnterUserAge] = useState(0);
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
@@ -14,9 +15,17 @@ const AddUser = (props) => {
       enteredUserName.trim().length === 0 ||
       enterUserAge.trim().length === 0
     ) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty value)",
+      });
       return;
     }
     if (+enterUserAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (>0)",
+      });
       return;
     }
     props.onAddUser(enteredUserName, enterUserAge);
@@ -32,12 +41,19 @@ const AddUser = (props) => {
     setEnterUserAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModal
-        title=" An error occured"
-        message="something went wrong"
-      ></ErrorModal>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        ></ErrorModal>
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
