509aea7a8c6a7b5757aa0e46f6847600846aabf9
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-25 15:16:57 +0530

    adding sidebar component

---
+++ practice project - project management/src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectSidebar from "./components/ProjectSidebar";
+
+function App() {
+  return (
+    <>
+     <ProjectSidebar></ProjectSidebar>
+    </>
+  );
+}
+
+export default App;
---
+++ practice project - project management/src/components/ProjectSidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProjectSidebar = () => {
+  return (
+   <aside>
+    <h2>Your Projects</h2>
+    <div><button>+ Add Project</button></div>
+    <ul></ul>
+   </aside>
+  )
+}
+
+export default ProjectSidebar
