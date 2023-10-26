f7e02c9741da7f6e9ac3f1408bac9f300803603f
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-26 18:57:28 +0530

    Styling Buttons & Inputs with Tailwind CSS

--- practice project - project management/src/components/Input.jsx
+++ practice project - project management/src/components/Input.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-const Input = ({label, textarea,...props }) => {
+const Input = ({ label, textarea, ...props }) => {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
   return (
-    <p>
-      <label htmlFor="">{label}</label>
-      {textarea ? <textarea /> : <input {...props}/>}
+    <p className="flex flex-col gap-1 my-4">
+      <label htmlFor="" className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
+      {textarea ? <textarea className={classes} {...props} /> : <input className={classes} {...props} />}
     </p>
   );
 };
--- practice project - project management/src/components/NewProject.jsx
+++ practice project - project management/src/components/NewProject.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Input from "./Input";
 export const NewProject = () => {
   return (
-    <div>
-      <menu>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4 ">
         <li>
-          <button>cancel</button>
+          <button className="text-stone-800 hover:text-stone-950">cancel</button>
         </li>
         <li>
-          <button>save</button>
+          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:text-stone-950">save</button>
         </li>
       </menu>
       <div>
