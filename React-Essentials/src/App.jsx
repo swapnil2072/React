8de634f0e1f9f0f0e9aec7d8a3a947e819e79c0a
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-30 21:01:57 +0530

    storing components in files

--- React-Essentials/src/App.jsx
+++ React-Essentials/src/App.jsx
@@ -1,35 +1,11 @@
-import reactImg from "./assets/react-core-concepts";
 import { CORE_CONCEPTS } from "./Data";
+import Header from "./components/Header";
+import CoreConcepts from "./components/CoreConcepts";
 
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
 
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} />
-      <h1>React Essentials</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
 
-function CoreConcepts({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+
 
 function App() {
   return (
