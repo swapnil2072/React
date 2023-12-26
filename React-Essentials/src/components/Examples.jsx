20b4a0de109ed8fdcb7959d5fd090b4d48de7b40
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-26 16:55:08 +0530

    setting components styles dynamically

--- React-Essentials/src/components/Examples.jsx
+++ React-Essentials/src/components/Examples.jsx
@@ -31,9 +31,9 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
+        ButtonsContainer="menu"
         buttons={
           <>
-            {" "}
             <TabButton
               isSelected={selectedTopic === "components"}
               onClick={() => handleSelect("components")}
