c796a8c1b49cc0d3833578de7e9ce8f31e1d2c6d
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-26 20:49:41 +0530

    display player name and symbol

--- React_Essentials-deep_dive/tic-tac-toe/src/App.jsx
+++ React_Essentials-deep_dive/tic-tac-toe/src/App.jsx
@@ -1,5 +1,21 @@
 function App() {
-  <p>Coming soon...</p>
+  <p>Coming soon...</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players"></ol>
+        <li>
+          <span className="player-name">Player 1</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          {" "}
+          <span className="player-name">Player 2</span>
+          <span className="player-symbol">O</span>
+        </li>
+      </div>
+    </main>
+  );
 }
 
 export default App;
