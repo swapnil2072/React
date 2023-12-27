ba17bf91c1dcc0133a80cad60b6ebd6b87014542
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-27 16:23:33 +0530

    avoid intersecting state

--- React_Essentials-deep_dive/tic-tac-toe/components/GameBoard.jsx
+++ React_Essentials-deep_dive/tic-tac-toe/components/GameBoard.jsx
@@ -7,19 +7,19 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard({ onSelectSquare,activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedBoard;
+  //   });
 
-    onSelectSquare();
-  }
+  //   onSelectSquare();
+  // }
 
   return (
     <ol id="game-board">
--- React_Essentials-deep_dive/tic-tac-toe/src/App.jsx
+++ React_Essentials-deep_dive/tic-tac-toe/src/App.jsx
@@ -1,8 +1,10 @@
 import Player from "../components/Player";
 import GameBoard from "../components/GameBoard";
+import Log from "../components/Log";
 import { useState } from "react";
 
 function App() {
+  const [gameTurns,setGameTurns] = useState([])
   const [activePlayer, setActivePlayer] = useState("X");
 
   function handleSelectSquare() {
@@ -28,7 +30,8 @@ function App() {
           activePlayerSymbol={activePlayer}
         ></GameBoard>
       </div>
-    </main>
+      <Log></Log>
+    </main> 
   );
 }
 
