963424534a7d0f42f5c38a26831b201575bec549
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-27 13:07:34 +0530

    condition content and suboptimal way of updating state

--- React_Essentials-deep_dive/tic-tac-toe/components/Player.jsx
+++ React_Essentials-deep_dive/tic-tac-toe/components/Player.jsx
@@ -4,12 +4,14 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing(!isEditing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
+  let btnCaption = "Edit";
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
+    btnCaption = "Save";
   }
 
   return (
@@ -18,7 +20,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{isEditing ? "save" : "edit"}</button>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
   );
 }
