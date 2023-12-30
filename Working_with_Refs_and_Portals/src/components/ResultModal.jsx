6007c39e7ef895c522d16cb4a6f09cad70a89350
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-30 21:45:00 +0530

    using modal to render code as direct succeser of body

--- Working_with_Refs_and_Portals/src/components/ResultModal.jsx
+++ Working_with_Refs_and_Portals/src/components/ResultModal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(
   ({ targetTime, remainingTime, onReset }, ref) => {
@@ -17,8 +18,8 @@ const ResultModal = forwardRef(
       };
     });
 
-    return (
-      <dialog ref={dialog} className="result-modal">
+    return createPortal(
+      <dialog ref={dialog} className="result-modal" onClose={onReset}>
         {userLost && <h2>You Lost</h2>}
         {!userLost && <h2>Your Score:{score}</h2>}
         <p>
@@ -31,7 +32,8 @@ const ResultModal = forwardRef(
         <form method="dialog" onSubmit={onReset}>
           <button>Close</button>{" "}
         </form>
-      </dialog>
+      </dialog>,
+      document.getElementById("modal"),
     );
   },
 );
