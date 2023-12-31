258f47115400c75c6e1bcfd10f079d34fdac15d6
swapnil2072 <swapnilshinde909693@gmail.com>
2023-12-31 17:54:45 +0530

    conditional rendering of noProjectSelected component and newProject component

--- practice project - project management/src/components/NoProjectSelected.jsx
+++ practice project - project management/src/components/NoProjectSelected.jsx
@@ -2,7 +2,7 @@ import NoProjectImage from "../assets/no-projects.png";
 import React from "react";
 import Button from "./Button";
 
-const NoProjectSelected = () => {
+const NoProjectSelected = ({ onStartAddProject }) => {
   return (
     <div className="mt-24 text-center w-2/3">
       <img
@@ -13,9 +13,11 @@ const NoProjectSelected = () => {
       <h2 className="text-xl font-bold text-stone-500 my-4">
         No Project Selected
       </h2>
-      <p className="text-stone-400 mb-4">Select a project or get started with one </p>
+      <p className="text-stone-400 mb-4">
+        Select a project or get started with one{" "}
+      </p>
       <p className="mt-4">
-        <Button >Create new project</Button>
+        <Button onClick={onStartAddProject}>Create new project</Button>
       </p>
     </div>
   );
