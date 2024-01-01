fe5d7d90953bc02c2021d40b4cee71f40e57c34f
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-01 20:09:12 +0530

    using context api

--- React's_Context_API_and_useReducer/src/store/shopping-cart-context.jsx
+++ React's_Context_API_and_useReducer/src/store/shopping-cart-context.jsx
@@ -2,7 +2,6 @@ import { createContext } from "react";
 
 export const CartContext = createContext({
   items: [],
-  addItemToCart: () =>{}
+  addItemToCart: () => {},
+  updateItemQuantity: () => {},
 });
-
-
