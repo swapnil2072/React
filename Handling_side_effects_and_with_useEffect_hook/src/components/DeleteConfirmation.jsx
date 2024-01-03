1f6b2a705f34a3a232af05760dd4252cf8da1b83
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-03 21:06:18 +0530

    optimazing state updates

--- Handling_side_effects_and_with_useEffect_hook/src/components/DeleteConfirmation.jsx
+++ Handling_side_effects_and_with_useEffect_hook/src/components/DeleteConfirmation.jsx
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
+import ProgressBar from "./ProgressBar";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
   useEffect(() => {
     console.log("TIMER SET");
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
     return () => {
       console.log("Cleaning up timer");
@@ -25,6 +28,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
---
+++ Handling_side_effects_and_with_useEffect_hook/src/components/ProgressBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState,useEffect } from 'react';
+
+const ProgressBar = ({timer}) => {
+     const [remainingTime, setRemainingTime] = useState(timer);
+
+     useEffect(() => {
+       const interval = setInterval(() => {
+         setRemainingTime((prevTime) => prevTime - 10);
+       }, 10);
+       return () => {
+         clearInterval(interval);
+       };
+     }, []);
+  return <progress value={remainingTime} max={timer} />;
+}
+
+export default ProgressBar
