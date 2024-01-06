31fa58de69a55036235a8f4e43bc8e09304db8cf
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-06 17:04:02 +0530

    understanding callBack hook

---
+++ A_look_behind_the_scene_of_react/src/components/Counter/ConfigureCounter.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useState } from "react";
+
+const ConfigureCounter = ({ onSet }) => {
+  const [enteredNumber, setEnteredNumber] = useState(0);
+
+  function handleChange(event) {
+    setEnteredNumber(+event.target.value);
+  }
+
+  function handleSetClick() {
+    onSet(enteredNumber);
+    setEnteredNumber(0);
+  }
+
+  return (
+    <section id="configure-counter">
+      <h2>Set Counter</h2>
+      <input type="number" onChange={handleChange} value={enteredNumber} />
+      <button onClick={handleSetClick}>Set</button>
+    </section>
+  );
+};
+
+export default ConfigureCounter;
