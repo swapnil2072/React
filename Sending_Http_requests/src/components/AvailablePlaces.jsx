a7b7c11e5b3b25047383bda60f35b79d67f65513
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-07 19:35:31 +0530

    preparing app for data fetching

---
+++ Sending_Http_requests/src/components/AvailablePlaces.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Places from "./Places.jsx";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
