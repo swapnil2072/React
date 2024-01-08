62d44a072f8cd9969d1c68027f913f4a0b063fe3
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-08 21:24:51 +0530

    transforming fetched data

--- Sending_Http_requests/src/components/AvailablePlaces.jsx
+++ Sending_Http_requests/src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -14,19 +15,27 @@ export default function AvailablePlaces({ onSelectPlace }) {
       try {
         const response = await fetch("http://localhost:3000/places");
         const resData = await response.json();
-        setAvailablePlaces(resData.places);
 
         if (!response.ok) {
           throw new Error("failed to fetch the data.");
         }
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude,
+          );
+          setAvailablePlaces(resData.places);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({
           message:
             error.message || "Could not fetch places,please try again later.",
         });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
