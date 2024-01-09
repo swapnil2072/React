d1ccda500246a6c9df8960b32189b4068d4b2121
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-09 22:01:28 +0530

    sending data with post request

--- Sending_Http_requests/src/http.js
+++ Sending_Http_requests/src/http.js
@@ -7,3 +7,18 @@ export async function fetchAvailablePlaces() {
   }
   return resData.places;
 }
+
+export default async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places: places }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("failed to update user data.");
+  }
+  return resData.message;
+}
