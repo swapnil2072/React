c7b7e9e0a4c99a10b6f43ca8b32ad651477d6707
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-10 22:05:08 +0530

    managing and returning state in custom hook

--- Building_Custom_Hooks/src/hooks/useFetch.js
+++ Building_Custom_Hooks/src/hooks/useFetch.js
@@ -1,21 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-function useFetch() {
+export function useFetch(fetchFn,initialValue) {
+  const [fetchedData, setFetchedData] = useState(initialValue);
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+
   useEffect(() => {
-    async function fetchPlaces() {
+    async function fetchData() {
       setIsFetching(true);
       try {
-        const places = await fetchUserPlaces();
-        setUserPlaces(places);
+        const data = await fetchFn();
+        setFetchedData(data);
       } catch (error) {
         setError({
-          message: error.message || "Failed to fetch user places.",
+          message: error.message || "Failed to fetch data.",
         });
       }
 
       setIsFetching(false);
     }
 
-    fetchPlaces();
-  }, []);
+    fetchData();
+  }, [fetchFn]);
+
+  return {
+    isFetching,
+    error,
+    fetchedData,
+  };
 }
