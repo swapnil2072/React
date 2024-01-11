b95695bf6c174c4570d306d719605ecafb740102
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-11 21:24:17 +0530

    mixing custom and build-in validation logic

--- Working_with_Forms_and_User_input/src/components/Signup.jsx
+++ Working_with_Forms_and_User_input/src/components/Signup.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function Signup() {
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -6,6 +10,10 @@ export default function Signup() {
     const acquisitionChannel = fd.getAll("acquisition");
     const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionChannel;
+    if (data.password !== data["confirm-password"]) {
+      setPasswordsAreNotEqual(true);
+      return;
+    }
     console.log(data);
     // event.target.reset(); programmatic way to reset form
   }
@@ -39,6 +47,9 @@ export default function Signup() {
             name="confirm-password"
             required
           />
+          <div className="control-error">
+            {passwordsAreNotEqual && <p>password must match</p>}
+          </div>
         </div>
       </div>
 
