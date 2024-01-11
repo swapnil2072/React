fc489a6ca0dfa6593dedd7d86d6ecfd185337a9c
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-11 19:55:59 +0530

    Validating Input on Every Keystroke via State

--- Working_with_Forms_and_User_input/src/components/StateLogin.jsx
+++ Working_with_Forms_and_User_input/src/components/StateLogin.jsx
@@ -7,6 +7,10 @@ export default function Login() {
     email: "",
     password: "",
   });
+
+  const emailIsInvalid =
+    enteredValue.email !== "" && !enteredValue.email.includes("@");
+
   function handleSubmit(event) {
     event.preventDefault();
     setEnteredValue({
@@ -37,6 +41,9 @@ export default function Login() {
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValue.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
