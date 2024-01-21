8870751deb65a6b2200e8b88d0afa0c930e9663c
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-21 19:09:51 +0530

    defining routes

---
+++ Routing/src/App.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+
+function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
