91309c4e81491a48e61c6a0f3af1f0306105b5ee
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-21 19:59:17 +0530

    working with NavLink

--- Routing/src/components/MainNavigation.js
+++ Routing/src/components/MainNavigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
@@ -7,11 +7,26 @@ const MainNavigation = () => {
     <header className={classes.header}>
       <ul className={classes.list}>
         <li>
-          <Link to="/">Home</Link>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+            end
+          >
+            Home
+          </NavLink>
         </li>
         <li>
           {" "}
-          <Link to="/products">Products</Link>
+          <NavLink
+            to="/products"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+          >
+            Products
+          </NavLink>
         </li>
       </ul>
     </header>
