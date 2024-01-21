5d73ab5310e815c35592b32a18f5e134d97c92be
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-21 17:37:46 +0530

    fetching logic

--- Advanced_Redux/src/App.js
+++ Advanced_Redux/src/App.js
@@ -24,7 +24,10 @@ function App() {
       isInitial = false;
       return;
     }
-    dispatch(sendCartData(cart));
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
--- Advanced_Redux/src/store/cart-actions.js
+++ Advanced_Redux/src/store/cart-actions.js
@@ -1,7 +1,6 @@
 import { cartActions } from "./cart-slice";
 import { uiActions } from "./ui-slice";
 
-
 export const fetchCartData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
@@ -17,7 +16,12 @@ export const fetchCartData = () => {
     };
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        }),
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -45,7 +49,10 @@ export const sendCartData = (cart) => {
         "https://react-redux-dcf25-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         },
       );
       if (!response.ok) {
