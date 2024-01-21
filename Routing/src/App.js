13ac9930a35aacfd65966fbea966b2900cb1e243
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-21 19:51:09 +0530

    showing Error pages with errorElement

--- Routing/src/App.js
+++ Routing/src/App.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       { path: "/", element: <Home /> },
       { path: "/products", element: <Products /> },
