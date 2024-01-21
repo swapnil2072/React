ab327fd6fc7d539a746e7545bda38aecdb413c14
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-21 19:20:45 +0530

    navigating between pages with Link

--- Routing/src/pages/Home.js
+++ Routing/src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <h1>My home page</h1>
-  )
+    <>
+      <h1>My home page</h1>
+      <p>
+        Go to <Link to="/products">The list of products</Link>
+      </p>
+    </>
+  );
 }
 
 export default Home
