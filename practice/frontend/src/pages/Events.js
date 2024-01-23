e11f722f8a10fa5dcc36083b256b9cbab5b589a4
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-23 18:07:10 +0530

    json utility function

--- practice/frontend/src/pages/Events.js
+++ practice/frontend/src/pages/Events.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
@@ -17,13 +17,14 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/eventss");
+  const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     // return { isError: true, message: "could not fetch Event" };
-    throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
-      status: 500,
-    });
+    // throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
+    //   status: 500,
+    // });
+    return json({ message: "Could not fetch events" }, { status: 500 });
   } else {
     return response;
   }
