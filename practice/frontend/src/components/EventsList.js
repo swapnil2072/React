dee992d042a849d82b07489f74b9a5b21071a8aa
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-24 14:56:17 +0530

    updated ui of home page

--- practice/frontend/src/components/EventsList.js
+++ practice/frontend/src/components/EventsList.js
@@ -1,9 +1,9 @@
 // import { useLoaderData } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import classes from './EventsList.module.css';
+import classes from "./EventsList.module.css";
 
-function EventsList({events}) {
+function EventsList({ events }) {
   // const events = useLoaderData();
 
   return (
@@ -12,7 +12,7 @@ function EventsList({events}) {
       <ul className={classes.list}>
         {events.map((event) => (
           <li key={event.id} className={classes.item}>
-            <Link to={event.id}>
+            <Link to={`/events/${event.id}`}>
               <img src={event.image} alt={event.title} />
               <div className={classes.content}>
                 <h2>{event.title}</h2>
