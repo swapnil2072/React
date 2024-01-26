0632ef11c8c838aab1caa7f020ae8dae09dc5fea
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-26 18:17:19 +0530

    adding logout button

--- Authentication_in_React/frontend/src/App.js
+++ Authentication_in_React/frontend/src/App.js
@@ -13,7 +13,10 @@ import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
-import AuthenticationPage,{action as authAction} from './pages/Authentication'
+import AuthenticationPage, {
+  action as authAction,
+} from "./pages/Authentication";
+import { action as logoutAction } from "./pages/Logout";
 
 const router = createBrowserRouter([
   {
@@ -55,11 +58,15 @@ const router = createBrowserRouter([
           },
         ],
       },
-      { path: "auth", element: <AuthenticationPage />,action:authAction },
+      { path: "auth", element: <AuthenticationPage />, action: authAction },
       {
         path: "newsletter",
         element: <NewsletterPage />,
         action: newsletterAction,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
       },
     ],
   },
