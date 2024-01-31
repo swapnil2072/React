5d0bf4f8e5a98418322756cb363744f0fbdb0e54
swapnil2072 <swapnilshinde909693@gmail.com>
2024-01-31 07:30:59 +0530

    commit

--- React Query/src/components/Events/EventDetails.jsx
+++ React Query/src/components/Events/EventDetails.jsx
@@ -1,8 +1,39 @@
-import { Link, Outlet } from 'react-router-dom';
-
-import Header from '../Header.jsx';
+import { Link, Outlet, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import Header from "../Header.jsx";
+import { fetchEvent } from "../../util/http.js";
 
 export default function EventDetails() {
+  const params = useParams();
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div id="event-details-content" className="center">
+        <p>Fetching event data...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <div id="event-details-content" className="center">
+        <ErrorBlock
+          title="Failed to load event"
+          message={
+            error.info?.message ||
+            "Failed to fetch event data,please try again later."
+          }
+        ></ErrorBlock>
+      </div>
+    );
+  }
   return (
     <>
       <Outlet />
