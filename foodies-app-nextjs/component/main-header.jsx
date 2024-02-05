46c73df14835f9f58f7ead0f934ccc589914c84f
swapnil2072 <swapnilshinde909693@gmail.com>
2024-02-05 21:01:35 +0530

    styling nextjs project using css module

--- foodies-app-nextjs/component/main-header.jsx
+++ foodies-app-nextjs/component/main-header.jsx
@@ -1,20 +1,25 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
+import classes from "@/component/main-header.module.css";
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
         <img src={logoImg.src} alt="logo of website" />
         NextLevel Food
       </Link>
-      <nav>
-        <ul>
+      <nav className={classes.nav}>
+        <ul className={classes.ul}>
           <li>
-            <Link href="/meals">Browse Meals</Link>
+            <Link href="/meals" className={classes.a}>
+              Browse Meals
+            </Link>
           </li>
           <li>
-            <Link href="/community">Foodies Community</Link>
+            <Link href="/community" className={classes.a}>
+              Foodies Community
+            </Link>
           </li>
         </ul>
       </nav>
