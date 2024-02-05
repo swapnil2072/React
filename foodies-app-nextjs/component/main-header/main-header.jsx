dbaf091f8e1109c349d5cd2e0aab6e39c18c9058
swapnil2072 <swapnilshinde909693@gmail.com>
2024-02-05 21:22:02 +0530

    adding more custom components

---
+++ foodies-app-nextjs/component/main-header/main-header.jsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import classes from "@/component/main-header/main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "@/component/main-header/main-header-background";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground></MainHeaderBackground>
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image
+            src={logoImg.src}
+            height={1000}
+            width={1000}
+            alt="logo of website"
+            priority
+          />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul className={classes.ul}>
+            <li>
+              <Link href="/meals" className={classes.a}>
+                Browse Meals
+              </Link>
+            </li>
+            <li>
+              <Link href="/community" className={classes.a}>
+                Foodies Community
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
