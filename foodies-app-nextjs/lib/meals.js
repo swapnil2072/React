64a7a88711d2920f9c9362c042f539034bce5331
swapnil2072 <swapnilshinde909693@gmail.com>
2024-02-11 14:56:05 +0530

    Loading & Rendering Meal Details via Dynamic Routes & Route Parameters

--- foodies-app-nextjs/lib/meals.js
+++ foodies-app-nextjs/lib/meals.js
@@ -6,6 +6,9 @@ export async function getMeals() {
   await new Promise((resolve) => {
     setTimeout(resolve, 2000);
   });
-  throw new Error("Loading meals failed");
   return db.prepare("SELECT * FROM meals").all();
+}
+
+export function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
