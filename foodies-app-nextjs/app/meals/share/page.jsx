c7134d4d1097f6b5df7ea52f585119291b218b2c
swapnil2072 <swapnilshinde909693@gmail.com>
2024-02-12 22:41:01 +0530

    adding server side validations

--- foodies-app-nextjs/app/meals/share/page.jsx
+++ foodies-app-nextjs/app/meals/share/page.jsx
@@ -1,6 +1,7 @@
 import ImagePicker from "@/component/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/component/meals/meals-form-submit";
 
 export default function ShareMealPage() {
   return (
@@ -42,7 +43,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image"></ImagePicker>
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />{" "}
           </p>
         </form>
       </main>
--- foodies-app-nextjs/lib/actions.js
+++ foodies-app-nextjs/lib/actions.js
@@ -1,6 +1,11 @@
 "use server";
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
+
+function isInvalidText(text) {
+  return !text || text.trim() === "";
+}
+
 export async function shareMeal(formData) {
   const meal = {
     title: formData.get("title"),
@@ -10,6 +15,19 @@ export async function shareMeal(formData) {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error("invalid input.");
+  }
   await saveMeal(meal);
   redirect("/meals");
 }
