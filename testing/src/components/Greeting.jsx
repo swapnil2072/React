c568fd865f1bcc58ad820d79913a3f20c5c409ce
swapnil2072 <swapnilshinde909693@gmail.com>
2024-02-19 18:21:11 +0530

    testing user interactions and states

--- testing/src/components/Greeting.jsx
+++ testing/src/components/Greeting.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useState } from "react";
 
 const Greeting = () => {
+  const [changedTExt, setChangedTExt] = useState(false);
+
+  function changeTextHandler() {
+    setChangedTExt(true);
+  }
+
   return (
     <div>
       <h2>Hello World</h2>
-      <p>It's good to see you!</p>
+      {!changedTExt && <p>It's good to see you!</p>}
+      {changedTExt && <p>Changed!</p>}
+      <button onClick={changeTextHandler}>Change Text</button>
     </div>
   );
 };
