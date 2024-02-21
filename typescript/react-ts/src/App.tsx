771a705dbc10a366b4cf743b06b16a4bac75640f
swapnil2072 <swapnilshinde909693@gmail.com>
2024-02-21 13:08:34 +0530

    working with function props

--- typescript/react-ts/src/App.tsx
+++ typescript/react-ts/src/App.tsx
@@ -1,17 +1,18 @@
 import Todos from "./components/Todos";
 import "./App.css";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript')
-  ];
-  return (
-    <div>
-      <Todos items={todos}></Todos>
-    </div>
-  );
+    const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+
+    const addTodoHandler = (todoText: string) => {};
+    return (
+        <div>
+            <NewTodo onAddTodo={addTodoHandler}></NewTodo>
+            <Todos items={todos}></Todos>
+        </div>
+    );
 }
 
 export default App;
--- typescript/react-ts/src/components/NewTodo.tsx
+++ typescript/react-ts/src/components/NewTodo.tsx
@@ -1,20 +1,20 @@
 import React, { useRef } from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     const todoTextInputRef = useRef<HTMLInputElement>(null);
     const submitHandler = (event: React.FormEvent) => {
         event?.preventDefault();
         const enteredText = todoTextInputRef.current!.value;
-        if(enteredText.trim().length===0){
-            return ;
+        if (enteredText.trim().length === 0) {
+            return;
         }
-        
+        props.onAddTodo(enteredText);
     };
 
     return (
         <form action="" onSubmit={submitHandler}>
             <label htmlFor="text">Todo Text</label>
-            <input type="text" id="text" ref={todoTextInputRef}   />
+            <input type="text" id="text" ref={todoTextInputRef} />
             <button type="button">Add Todo</button>
         </form>
     );
