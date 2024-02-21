3cbee4717e700098f9d4c202e47ae2552f82c644
swapnil2072 <swapnilshinde909693@gmail.com>
2024-02-21 13:34:50 +0530

    added styling

--- typescript/react-ts/src/components/NewTodo.tsx
+++ typescript/react-ts/src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import classes from "./NewTodo.module.css";
 
 const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     const todoTextInputRef = useRef<HTMLInputElement>(null);
@@ -12,10 +13,17 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     };
 
     return (
-        <form onSubmit={submitHandler}>
-            <label htmlFor="text">Todo Text</label>
-            <input type="text" id="text" ref={todoTextInputRef} />
-            <button>Add Todo</button>
+        <form onSubmit={submitHandler} className={classes.form}>
+            <label htmlFor="text" className={classes.label}>
+                Todo Text
+            </label>
+            <input
+                type="text"
+                id="text"
+                ref={todoTextInputRef}
+                className={classes.input}
+            />
+            <button className={classes.button}>Add Todo</button>
         </form>
     );
 };
--- typescript/react-ts/src/components/TodoItem.tsx
+++ typescript/react-ts/src/components/TodoItem.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import classes from "./TodoItems.module.css";
 
-const TodoItem : React.FC<{text:string}> = (props) => {
-  return <li>{props.text}</li>;
+const TodoItem: React.FC<{ text: string }> = (props) => {
+    return <li className={classes.item}>{props.text}</li>;
 };
 
 export default TodoItem;
